4a491d207918e7697e82587a6b1f9f4083b608f3
Sriram <sriramsubramanian23@gmail.com>
2024-03-02 14:02:46 +0530

    Implemented logo and favicon

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,10 +2,12 @@ import PopularMovies from "../../components/molecules/popular-movies";
 import SearchMovie from "../../components/molecules/search-movie";
 import Tabs from "../../components/tabs";
 import './home.css'
+import LOGO from '../../../assets/logo.png'
 const Home = () => {
   return (
     <div className="home-container">
-      <div className="flex flex-col md:flex-row p-2 justify-between items-center">
+      <div className="flex flex-col md:flex-row p-2 items-center">
+        <img src={LOGO} alt="Cine Corner Logo" className="w-16 m-2" />
         <h1 className="text-4xl mb-4 pt-4 font-bold">Cine Corner</h1>
       </div>
       <Tabs
